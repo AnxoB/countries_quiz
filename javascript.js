4a9832e9e2158f2307481f1c3a62ac24a4a4eddd
dam1 - Búa Alonso <A22AnxoBA@sanclemente.local>
2025-01-29 14:24:51 +0100

    Version 0.2

--- javascript.js
+++ javascript.js
@@ -1,3 +1,17 @@
-function next(){
-    
+let contador = 1;
+
+function next() {
+    document.getElementById(contador).classList.add('d-none');
+
+    contador++;
+
+    document.getElementById(contador).classList.remove('d-none');
+}
+
+function previous() {
+    document.getElementById(contador).classList.add('d-none');
+
+    contador--;
+
+    document.getElementById(contador).classList.remove('d-none');
 }
