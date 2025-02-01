b550b15bcda97963dce492478c9a97ff5b3501c8
Anxo Bua Alonso <a22anxoba@iessanclemente.net>
2025-02-01 18:11:53 +0100

    Version 0.3

--- javascript.js
+++ javascript.js
@@ -2,16 +2,13 @@ let contador = 1;
 
 function next() {
     document.getElementById(contador).classList.add('d-none');
-
     contador++;
-
     document.getElementById(contador).classList.remove('d-none');
 }
 
 function previous() {
     document.getElementById(contador).classList.add('d-none');
-
     contador--;
-
     document.getElementById(contador).classList.remove('d-none');
 }
+
